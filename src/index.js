c35f5fb11feb054fef9e734f77ac06a14a08570a
Aleksandr Skirnevskyi <sanya-sha2016@ukr.net>
2021-03-10 20:11:12 +0200

    df

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+    return array && array.length != 0 ? Math.min(...array) : 0;
+  }
+  
+  exports.max = function max (array) {
+    return array && array.length != 0 ? Math.max(...array) : 0;
+  }
+  
+  exports.avg = function avg (array) {
+        if (array && array.length != 0) {
+          const arraySum = array.reduce((previousValue, currentValue) => {
+              return previousValue + currentValue;
+          });
+  
+          return arraySum / array.length;
+      } else {
+          return 0;
+      }
+  }
